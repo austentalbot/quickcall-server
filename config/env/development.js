f111ba108491e9c667998b9b2c9332b1b0474ad1
dhfromkorea <dhllee424@gmail.com>
2014-07-10 15:19:18 -0700

    excluded env dev"

--- config/env/development.js
+++ config/env/development.js
@@ -2,8 +2,6 @@ module.exports = {
   db: 'mongodb://localhost/your_project_development',
   google: {
     clientID:'114383978829-8mgiisdf7a1l8g1pigt37ggjolqdlj1l.apps.googleusercontent.com',
-// '267057860316-qjdth18861rb2h8j4pnsupv03pipjagq.apps.googleusercontent.com',
-    // consumerSecret: 'd1SHrhiP_-jS1SITy46gwK5t',
     clientSecret: 'Bmw2epStxC9nCEKo-vn6M3Vq',
     callbackURL: 'http://localhost:3000/',
     scope: [
@@ -13,12 +11,9 @@ module.exports = {
     
   },
   plivo: {
-<<<<<<< HEAD
       "authId": "MAZDFKYJA1OTU2M2NKMG",
       "authToken": "YzA0ZWJkOGJlYzQyNGMyMWEyMmRmODdkYTcyM2Rl"
-=======
     "authId": process.env.plivo_auth_id,
     "authToken": process.env.plivo_auth_token
->>>>>>> set up the heroku deployable
   }
 };
