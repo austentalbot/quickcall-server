cd38b4379aff5b549dfe6fe180468a4d79017fc0
dhfromkorea <dhllee424@gmail.com>
2014-07-11 16:50:12 -0700

    git commit

--- config/env/development.js
+++ config/env/development.js
@@ -1,16 +1,18 @@
 module.exports = {
-  db: 'mongodb://localhost/your_project_development',
-  google: {
-    clientID: '267057860316-qjdth18861rb2h8j4pnsupv03pipjagq.apps.googleusercontent.com',
-    clientSecret: 'd1SHrhiP_-jS1SITy46gwK5t',
-    callbackURL: 'http://quickcall-server.herokuapp.com/auth/google/callback',
-    scope: [
-      'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email'
-    ]
-  },
-  plivo: {
-      "authId": "MAZDFKYJA1OTU2M2NKMG",
-      "authToken": "YzA0ZWJkOGJlYzQyNGMyMWEyMmRmODdkYTcyM2Rl"
-  }
+    db: process.env.MONGOLAB_URI ||
+        process.env.MONGOHQ_URL ||
+        'mongodb://localhost/quickcall',
+    google: {
+        clientID: '267057860316-qjdth18861rb2h8j4pnsupv03pipjagq.apps.googleusercontent.com',
+        clientSecret: 'd1SHrhiP_-jS1SITy46gwK5t',
+        callbackURL: 'http://quickcall-server.herokuapp.com/auth/google/callback',
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ]
+    },
+    plivo: {
+        "authId": "MAZDFKYJA1OTU2M2NKMG",
+        "authToken": "YzA0ZWJkOGJlYzQyNGMyMWEyMmRmODdkYTcyM2Rl"
+    }
 };
