a3ce2cf1453b4f7a2813b43f5c12eced79dc387d
dhfromkorea <dhllee424@gmail.com>
2014-07-11 11:25:20 -0700

    merge conflict on passport-google resolved

--- utility/routes.js
+++ utility/routes.js
@@ -1,25 +1,15 @@
 var mongoose = require('mongoose');
-var phone = require('call');
+var phone = require('./call');
 var config = require('config');
 
 
 module.exports = function(app, passport) {
   app.post('/call', function(req, res) {
-   phone.makecalls(req, res);
+   phone.initialCall(req, res);
   });
 
   app.post('/xml-response', function(req, res) {
-    var dstNum = req.body.dst;
-    // should get the number to call
-    var r = new plivo.Response();
-    var dialElement = r.addDial();
-    // get the target number
-    dialElement.addNumber(dstNum);
-    r.addSpeak('Awesome', {
-      loop: 2
-    });
-    var xmlRes = r.toXML();
-    res.send(200, xmlRes);
+   phone.connectCall(req, res);
   });
 
   app.post('/login', passport.authenticate('google', {
