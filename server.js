288c45c9bb5c5e0b2fa8e224996dead9e3cae80b
dhfromkorea <dhllee424@gmail.com>
2014-07-10 18:26:21 -0700

    enabled passport

--- server.js
+++ server.js
@@ -26,5 +26,30 @@ require('./utility/passport.js')();
 require('./config/middleware.js')(app);
 //add routes
 require('./utility/routes')(app, passport, User);
+=======
+// Connect to mongodb
+// var connect = function () {
+//   var options = { server: { socketOptions: { keepAlive: 1 } } };
+//   mongoose.connect(config.db, options);
+// };
+// connect();
+
+// mongoose.connection.on('error', console.log);
+// mongoose.connection.on('disconnected', connect);
+
+// Bootstrap models
+// fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
+//   if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
+// });
+
+// Bootstrap passport config
+require('./config/passport')(passport, config);
+
+// Bootstrap application settings
+require('./config/express')(app, passport);
+
+// Bootstrap routes
+require('./config/routes')(app, passport);
+
 app.listen(port);
 console.log('listening on',port);
