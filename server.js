c92aa06dfdd92c347b1cd470a344a8b922ee91ac
Yahkob <jakobharclerode@gmail.com>
2014-07-10 19:05:10 -0700

    mongo testing

--- server.js
+++ server.js
@@ -12,16 +12,35 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-// Connect to mongodb
-// var connect = function () {
-//   var options = { server: { socketOptions: { keepAlive: 1 } } };
-//   mongoose.connect(config.db, options);
-// };
-// connect();
 
-// mongoose.connection.on('error', console.log);
-// mongoose.connection.on('disconnected', connect);
+var connect = function () {
+  var options = { server: { socketOptions: { keepAlive: 1 } } };
+  mongoose.connect(config.db, options);
+};
+connect();
 
+mongoose.connection.on('error', console.log);
+mongoose.connection.on('disconnected', connect);
+
+
+
+var Schema = mongoose.Schema;
+/**
+ * User schema
+ */
+var UserSchema = new Schema({
+  username: { type: String, required: true, unique: true},
+  phonenumber: { type: Number, required: true, unique: true}
+  // preferences: {}
+});
+var User = mongoose.model('User', UserSchema);
+new User({username:'test',phonenumber:'test'}).save(function(err,e){
+	if(err) {
+		return error;
+	}
+	return e;
+});
+User.find()
 // Bootstrap models
 // fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
 //   if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
