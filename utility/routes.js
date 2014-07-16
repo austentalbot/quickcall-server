fb7c67ac8ddbfe58bbf51b0b41d1bc6261d04505
Yahkob <jakobharclerode@gmail.com>
2014-07-16 13:43:12 -0700

    pull request

--- utility/routes.js
+++ utility/routes.js
@@ -2,10 +2,14 @@ var phone = require('./call');
 
 
 module.exports = function(app) {
+  app.get('/', function(req,res){
+    res.send('Home');
+  });
   app.post('/call', function(req, res) {
    phone.callSrcNum(req, res);
   });
   app.post('/xml-response', function(req, res) {
+
     phone.callDstNum(req, res);
   });
 };
