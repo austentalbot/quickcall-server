1d48145ab722c8affba471098cd5a04451546e99
dhfromkorea <dhllee424@gmail.com>
2014-07-18 11:43:32 -0700

    commented the code

--- utility/call.js
+++ utility/call.js
@@ -1,46 +1,56 @@
+// to connect voice calls, we are relying on the Plivo API and its Nodejs library
+// for more details: https://github.com/plivo/plivo-examples-node
+
 var plivo = require('plivo');
 
+// this is required to make requests to plivo
+// the auth pair is tied to the user's account and remaining $ credit
+// so plivo will handle the payment instead of us
+var initializePlivo = function(req) {
+    var credentials = {
+        authId: req.body.authId || req.query.authId,
+        authToken: req.body.authToken || req.query.authToken
+    };
+    return plivo.RestAPI(credentials);
+};
+
+// calling the app user's number (src)
+// when a call is connected, plivo will make a GET request to answer_url (callback)
+// expecting an xml instruction to call the app user's friend (dst)
+// the from property in params obj is where you set the callerID
+// check https://www.plivo.com/docs/api/call/
 exports.callSrcNum = function(req, res) {
-    // ask plivo to call the app user
     var params = {
         from: req.body.src,
         to: req.body.src,
         answer_url: "http://quickcall-server.herokuapp.com/xml-response?dst=" + req.body.dst
     };
-    // default fallback to DH's API credentials (temporary)
-    var credentials = {
-        authId: req.body.authId || req.query.authId,
-        authToken: req.body.authToken || req.query.authToken
-    };
-    var p = plivo.RestAPI(credentials);
+    var p = initializePlivo(req);
     p.make_call(params, function(status, response) {
         res.send(status, response);
     });
 };
 
+// this will generate an xml doc which tells plivo to call the app user's friend (dst)
+// check https://www.plivo.com/docs/xml/
 exports.callDstNum = function(req, res) {
-    // provide an xml instruction which tells plivo what to do after a call gets connected
-    // ask plivo to call the person the app user wants to talk to
     var r = new plivo.Response();
     var dialElement = r.addDial();
     dialElement.addNumber(req.body.dst);
     res.send(200, r.toXML());
 };
 
+// this is needed to fetch our user account details from Plivo
+// check https://www.plivo.com/docs/api/account/
 exports.getAccountDetails = function(req, res) {
-    // should use req.query, not req.body to parse the query string input from a GET request
-    var credentials = {
-        authId: req.body.authId || req.query.authId,
-        authToken: req.body.authToken || req.query.authToken
-    };
-    var p = plivo.RestAPI(credentials);
-    // send an empty obj as it's expected
+    var p = initializePlivo(req);
     p.get_account({}, function(status, response) {
         res.send(status, response);
     });
 };
 
 
+// future roadmap if you were to add an SMS functionality
 // exports.sendSMS = function(req, res){
 //     var params = {};
 // };
