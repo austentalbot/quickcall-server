265fb8e15a6acfe2c4f5e9af98e19d152310352a
Jakob Harclerode <jakobharclerode@gmail.com>
2014-07-21 10:35:39 -0700

    Made comment format more readable

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
-// currently this is not being used but later in the future, if we go // down the route of adding the payment gateway into the mobile app.
-// we will need to route all calls via a central account. at that time // we will need this file and /env so keep them here
+/* currently this is not being used but later in the future, if we go down the route
+of adding the payment gateway into the mobile app.
+we will need to route all calls via a central account. at that time we will need this file and /env so keep them here
+*/
 module.exports = {
   development: require('./env/development') || '',
   production: require('./env/production'),
