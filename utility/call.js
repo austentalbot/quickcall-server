c52d2ffeae2ff18ecbffe4c3dad427426e9351cc
Austen Talbot <austentalbot@gmail.com>
2014-07-24 15:09:31 -0700

    Save master account credentials to be used to create subaccounts

--- utility/call.js
+++ utility/call.js
@@ -76,8 +76,8 @@ exports.forwardSMS = function(req, res) {
 // this function is not yet hooked up, but it is ready to go once the DB and front-end are ready
 // we also need to upgrade the account to allow for multiple numbers
 exports.createNewUser = function(req, res) {
-    // var p = plivo.RestAPI({authId: INSERTAUTHID, authToken: INSERTAUTHTOKEN});
-    var p = initializePlivo(req);
+    var p = plivo.RestAPI({authId: credentials.masterAuthId, authToken: credentials.masterAuthToken});
+    // var p = initializePlivo(req);
     // var params = {
     //     name: 'test',
     //     enabled: true
